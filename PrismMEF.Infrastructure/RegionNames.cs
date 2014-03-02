666dfe13f76a4d68124eb61585a027941752f3b8
celsoaf <celsoaf@gmail.com>
2014-03-02 23:29:07 +0000

    add mef infrastructure

---
+++ PrismMEF.Infrastructure/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrismMEF.Infrastructure
+{
+    public static class RegionNames
+    {
+        public const string LeftRegion = "LeftRegion";
+        public const string RightRegion = "RightRegion";
+        public const string BottomRegion = "BottomRegion";
+    }
+}
