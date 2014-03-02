2c0c11dd3913e9cbdef5ef10ae2b256d4b577156
celsoaf <celsoaf@gmail.com>
2014-03-02 14:01:38 +0000

    add unity infrastructure

---
+++ PrismUnity.Infrastructure/RegionNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrismUnity.Infrastructure
+{
+    public static class RegionNames
+    {
+        public const string TopRegion = "TopRegion";
+        public const string TopRegion = "LeftRegion";
+        public const string TopRegion = "RightRegion";
+        public const string TopRegion = "BottomRegion";
+    }
+}
