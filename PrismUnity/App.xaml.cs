3f065aa561bac9bc6797af473fe30addae0f6e74
celsoaf <celsoaf@gmail.com>
2014-03-02 14:33:49 +0000

    Add bootstrapper

--- PrismUnity/App.xaml.cs
+++ PrismUnity/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
 
 namespace PrismUnity
 {
@@ -12,5 +13,15 @@ namespace PrismUnity
     /// </summary>
     public partial class App : Application
     {
+        private static UnityBootstrapper bootstrapper;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+
+        }
     }
 }
---
+++ PrismUnity/Bootstrapper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+using System.Windows;
+using PrismUnity.Views.Shell;
+using Microsoft.Practices.Prism.Modularity;
+using PrismUnity.Controllers;
+
+namespace PrismUnity
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog catalog = (ModuleCatalog)this.ModuleCatalog;
+
+            //catalog.AddModule(typeof(Backup.BackupModule));
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            var shell = Container.Resolve<IShellViewModel>();
+
+            var view = shell.View as Window;
+
+            view.Show();
+
+            return view;
+        }
+
+        protected override void ConfigureContainer()
+        {
+            registerTypes();
+
+            base.ConfigureContainer();
+        }
+
+        private void registerTypes()
+        {
+            Container.RegisterType<IPrismUnityController, PrismUnityController>(
+                new ContainerControlledLifetimeManager());
+
+            Container.RegisterType<IShellView, ShellView>();
+            Container.RegisterType<IShellViewModel, ShellViewModel>();
+
+            //Container.RegisterType<IRoadView, RoadView>();
+            //Container.RegisterType<ILeftView, LeftView>();
+
+            //Container.RegisterType<IRightView, RightView>();
+            //Container.RegisterType<ITopView, TopView>();
+
+            //Container.RegisterType<IRoadViewModel, RoadViewModel>();
+            //Container.RegisterType<ILeftViewModel, LeftViewModel>();
+            //Container.RegisterType<IRightViewModel, RightViewModel>();
+            //Container.RegisterType<ITopViewModel, TopViewModel>();
+        }
+
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+
+            Container.Resolve<IPrismUnityController>();
+        }
+    }
+}
