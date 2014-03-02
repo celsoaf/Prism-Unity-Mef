f7b18494be39c1761429e16f361ad02d32f00548
celsoaf <celsoaf@gmail.com>
2014-03-02 14:32:33 +0000

    Add controller

---
+++ PrismUnity/Controllers/PrismUnityController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+
+namespace PrismUnity.Controllers
+{
+    public class PrismUnityController : IPrismUnityController
+    {
+        private IUnityContainer _container;
+
+        public PrismUnityController(IUnityContainer container)
+        {
+            _container = container;
+        }
+    }
+}
