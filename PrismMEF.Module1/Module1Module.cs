eee63dbc1cc8ff7cb61f85b84dbec32855d5341c
celsoaf <celsoaf@gmail.com>
2014-03-03 01:04:44 +0000

    add mef module1module

---
+++ PrismMEF.Module1/Module1Module.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using PrismMEF.Module1.Controllers;
+using System.ComponentModel.Composition;
+
+namespace PrismMEF.Module1
+{
+    [ModuleExport(typeof(Module1Module))]
+    public class Module1Module : IModule
+    {
+        [ImportingConstructor]
+        public Module1Module(IModule1Controller controller)
+        {
+
+        }
+
+        public void Initialize()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
