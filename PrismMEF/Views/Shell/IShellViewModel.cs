18484fbeb54058ecf83c113ee73f4e4e481d65f3
celsoaf <celsoaf@gmail.com>
2014-03-03 00:36:10 +0000

    update mef shell

--- PrismMEF/Views/Shell/IShellViewModel.cs
+++ PrismMEF/Views/Shell/IShellViewModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace PrismMEF.Views.Shell
 {
     public interface IShellViewModel
     {
         IShellView View { get; set; }
+
+        ICommand ExitCommand { get; set; }
     }
 }
--- PrismMEF/Views/Shell/ShellViewModel.cs
+++ PrismMEF/Views/Shell/ShellViewModel.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Events;
+using PrismMEF.Infrastructure.Events;
 
 namespace PrismMEF.Views.Shell
 {
@@ -10,12 +14,17 @@ namespace PrismMEF.Views.Shell
     public class ShellViewModel : IShellViewModel
     {
         [ImportingConstructor]
-        public ShellViewModel(IShellView view)
+        public ShellViewModel(IShellView view, IEventAggregator eventAggregator)
         {
             View = view;
             View.DataContext = this;
+
+            ExitCommand = new DelegateCommand(() =>
+                eventAggregator.GetEvent<ExitApplicationEvent>().Publish(null));
         }
 
         public IShellView View { get; set; }
+
+        public ICommand ExitCommand { get; set; }
     }
 }
