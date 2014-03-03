ab509402472c07474f719b5338abeb2a9e77d45b
celsoaf <celsoaf@gmail.com>
2014-03-03 01:06:20 +0000

    add mef module2 project

---
+++ PrismMEF.Module2/Module2Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.MefExtensions.Modularity;
+
+namespace PrismMEF.Module2
+{
+    [ModuleExport(typeof(Module2Module))]
+    public class Module2Module : IModule
+    {
+        public void Initialize()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
