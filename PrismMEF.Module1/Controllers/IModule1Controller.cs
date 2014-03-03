85e1765f39370611898992191eb6596c3c08c48b
celsoaf <celsoaf@gmail.com>
2014-03-03 01:04:20 +0000

    add mef module1 controller

---
+++ PrismMEF.Module1/Controllers/IModule1Controller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrismMEF.Module1.Controllers
+{
+    public interface IModule1Controller
+    {
+    }
+}
---
+++ PrismMEF.Module1/Controllers/Module1Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using Microsoft.Practices.Prism.Regions;
+using PrismMEF.Module1.Models;
+using PrismMEF.Infrastructure;
+using System.Collections.ObjectModel;
+using PrismMEF.Module1.Views.List;
+
+namespace PrismMEF.Module1.Controllers
+{
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    [Export(typeof(IModule1Controller))]
+    public class Module1Controller : IModule1Controller
+    {
+        //private IUnityContainer _container;
+        private IRegionManager _regionManager;
+        private IListViewModel _list;
+
+        [ImportingConstructor]
+        public Module1Controller(
+            //IUnityContainer container,
+            IRegionManager regionManager,
+            IListViewModel list)
+        {
+            //_container = container;
+            _regionManager = regionManager;
+
+            _list = list;
+
+            _list.Items = new ObservableCollection<ListItem>();
+
+            _list.Items.Add(new ListItem { Id = 1, Name = "Item 1" });
+            _list.Items.Add(new ListItem { Id = 2, Name = "Item 2" });
+            _list.Items.Add(new ListItem { Id = 3, Name = "Item 3" });
+
+            _regionManager.RegisterViewWithRegion(
+                RegionNames.LeftRegion,
+                () => _list.View);
+        }
+    }
+}
